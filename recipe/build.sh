365ec2a3ebefc87fc00b32e267dececa23a5a6c8
ngam <67342040+ngam@users.noreply.github.com>
2022-10-26 22:41:07 -0400

    fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 set -ex
 python -m pip install --no-deps --ignore-installed tensorflow_metadata-${PKG_VERSION}-py3-none-any.whl
 # Remove the strict protobuf requirement. Conda will manage it.
-sed -i 's/^Requires-Dist: protobuf.*/Requires-Dist: protobuf/' ${SP_DIR}/tensorboard-${PKG_VERSION}.dist-info/METADATA
+sed -i 's/^Requires-Dist: protobuf.*/Requires-Dist: protobuf/' ${SP_DIR}/tensorflow_metadata-${PKG_VERSION}.dist-info/METADATA
