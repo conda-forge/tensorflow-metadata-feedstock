d33863687f9f53c6ad8959dceafb4f2ee26769e8
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-04 19:09:02 +0900

    Fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-{{ PYTHON }} -m pip install -vv --no-deps --no-build-isolation ./tensorflow_metadata-{{ version }}-py3-none-any.whl
+${PYTHON} -m pip install -vv --no-deps --no-build-isolation tensorflow_metadata-${PKG_VERSION}-py3-none-any.whl
 # Remove the strict protobuf requirement, conda will manage it
 sed -i 's/^Requires-Dist: protobuf.*/Requires-Dist: protobuf/' ${SP_DIR}/tensorflow_metadata-${PKG_VERSION}.dist-info/METADATA
