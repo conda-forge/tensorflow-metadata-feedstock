04f2d2f1f4eb4c858ccb6879ada9c6ad70ee752e
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-04 17:32:27 +0900

    Fix installation script

--- recipe/build.sh
+++
@@ -1,4 +0,0 @@
-set -ex
-python -m pip install --no-deps --ignore-installed tensorflow_metadata-${PKG_VERSION}-py3-none-any.whl
-# Remove the strict protobuf requirement. Conda will manage it.
-sed -i 's/^Requires-Dist: protobuf.*/Requires-Dist: protobuf/' ${SP_DIR}/tensorflow_metadata-${PKG_VERSION}.dist-info/METADATA
